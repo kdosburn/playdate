b1f9d271c9b0ad93483dc6694db6fe8b976bfb22
Kendra Osburn <kdosburn@gmail.com>
2016-06-27 16:01:23 -0700

    Add grumpy icon

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the playful app!"
+      flash[:success] = "Welcome to Meow Town!"
   		redirect_to @user
   	else
   		render 'new'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,17 @@
+require "rubygems"
+require "json"
+require "net/http"
+require "uri"
+
 module UsersHelper
+
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "grumpy_icon_50.jpg"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
